cdc6dcd08bad4897397509f93f478be19da30666
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2023-07-05 15:32:56 -0400

    trimmed down standard fcl call

--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -12,7 +12,6 @@ import LoadingIndicator from "../components/LoadingIndicator";
 import NoWalletsView from "../components/NoWalletsView";
 import WalletServiceCard from "../components/WalletServiceCard";
 import WalletDiscoveryWrapper from "../components/WalletDiscoveryWrapper";
-import {FlowApp} from "./Flow"
 
 export default function Auth() {
   return (
@@ -44,7 +43,7 @@ export default function Auth() {
       >
         Please choose a wallet to continue
       </Text>
-      <FlowApp />
+      <Button onPress={fcl.logIn} title="Log In"/>
 
       <fcl.ServiceDiscovery
         fcl={fcl}
